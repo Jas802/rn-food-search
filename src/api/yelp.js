9633de3e3f3b802d143cea42d7533f3687016a8f
Jas802 <jas.spencer414@gmail.com>
2021-01-20 15:08:05 -0500

    setup axios for yelp api

---
+++ src/api/yelp.js
@@ -0,0 +1,9 @@
+import axios from 'axiox';
+
+export default axios.create({
+  baseURL: 'https://api.yelp.com/v3/businesses',
+  headers: {
+    Authorization:
+      'Bearer dmQUsoudark7gGXI9WpY151oQyurG3BOzwy_oNsVzGkuWQuZJjsTTC67HnQo9k47IdvS5-f2H_1q2vFgR7FV4-tZqScrz2D7XZQ8JYmhptekT2SAgNySFDbq5-EBYHYx',
+  },
+});
