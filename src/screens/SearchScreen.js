d84fb8c9a5bc982dd4e217b531ef83d3946df30e
Jas802 <jas.spencer414@gmail.com>
2021-01-21 15:44:37 -0500

    added useEffect for component rendering

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -8,12 +8,12 @@ const SearchScreen = () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const searchYelp = async () => {
+  const searchYelp = async (searchTerm) => {
     try {
       const response = await yelp.get('/search', {
         params: {
           limit: 50,
-          term,
+          term: searchTerm,
           location: 'new york',
         },
       });
@@ -23,9 +23,18 @@ const SearchScreen = () => {
     }
   };
 
+  // invoke searchYelp when component renders
+  useEffect(() => {
+    searchYelp('pasta');
+  }, []);
+
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchYelp} />
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchYelp(term)}
+      />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
